b15714b2d446d58f730b79d4af8b9b60d2d350f1
Abhinav9p16 <thakurabhinav56@gmail.com>
2020-10-15 12:32:20 +0530

    Welcome to Generics

---
+++ src/main/java/PrintArray.java
@@ -0,0 +1,32 @@
+public class PrintArray {
+/*
+    public static void print(Integer[] arr){
+        for(int element : arr){
+            System.out.printf("%s, " ,element);
+        }
+        System.out.println();
+    }
+    public static void print(Double[] arr){
+        for(Double element : arr){
+            System.out.printf("%s, " ,element);
+        }
+        System.out.println();
+    }
+    public static void print(Character[] arr){
+        for(Character element : arr){
+            System.out.printf("%s, " ,element);
+        }
+        System.out.println();
+    }*/
+
+    public static void main(String args[]) {
+        System.out.println("Welcome to Generics ");
+       /* Integer[] intArray = { 1, 2, 3, 4, 5 };
+        Double[] doubleArray = { 1.1, 2.2, 3.3, 4.4 };
+        Character[] charArray = {'H','E','L','L','O'};
+        PrintArray.print(intArray);
+        PrintArray.print(doubleArray);
+        PrintArray.print(charArray);*/
+    }
+}
+

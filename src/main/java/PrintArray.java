bc3b9891998368fed58250a1448c169a37e3a3e2
Abhinav9p16 <thakurabhinav56@gmail.com>
2020-10-15 22:05:47 +0530

    Option

--- src/main/java/PrintArray.java
+++ src/main/java/PrintArray.java
@@ -24,20 +24,34 @@ public class PrintArray {
             System.out.printf("%s, " ,element);
         }System.out.println();
     }
+    public static <T extends Comparable<T>> T max(T... elements) {
+        T max = elements[0];
+        for (T element : elements) {
+            if (element.compareTo(max) > 0) {
+                max = element;
+            }
+        }
+        return max;
+    }
 
     public static void main(String args[]) {
         System.out.println("Welcome to Generics ");
-        Integer[] intArray = { 1, 2, 3, 4, 5 };
+        Integer[] intArray = { 1, 2, 5, 4, 3 };
         Double[] doubleArray = { 1.1, 2.2, 3.3, 4.4 };
         Character[] charArray = {'H','E','L','L','O'};
         String[] strArray = {"Apple", "Peach","Banana"};
         new PArray<>(intArray).print();
         new PArray<>(doubleArray).print();
+        System.out.println("Integer Max: " + max(Integer.valueOf(32), Integer.valueOf(56), Integer.valueOf(89), Integer.valueOf(3), Integer.valueOf(456), Integer.valueOf(78), Integer.valueOf(45)));
+        System.out.println("Double Max: " + max(Double.valueOf(5.6), Double.valueOf(7.8), Double.valueOf(2.9), Double.valueOf(18.6), Double.valueOf(10.25), Double.valueOf(18.6001)));
+        System.out.println("String Max: " + max("Strawberry", "Mango", "Apple", "Pomegranate", "Guava", "Blackberry", "Cherry", "Orange", "Date"));
+        System.out.println("Boolean Max: " + max(Boolean.TRUE, Boolean.FALSE));
         new PArray<>(intArray).getMaxElement(intArray);
         new PArray<>(doubleArray).getMaxElement(doubleArray);
         new PArray<>(strArray).getMaxElement(strArray);
         PrintArray.print(doubleArray);
         PrintArray.print(charArray);
+
 
     }
 }

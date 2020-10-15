4d864184b95c783fdc3962a1001631a431803504
Abhinav9p16 <thakurabhinav56@gmail.com>
2020-10-15 12:55:31 +0530

    Generic Class

--- src/main/java/PrintArray.java
+++ src/main/java/PrintArray.java
@@ -1,3 +1,18 @@
+class PArray<T>{
+  private T[] arr;
+  PArray(T[] arr){
+      this.arr = arr ;
+  }
+  public void print(){
+      PArray.print(this.arr);
+  }
+    public static <E> void print(E[] arr){
+        for(E element : arr){
+            System.out.printf("%s, " ,element);
+        }System.out.println();
+    }
+}
+
 public class PrintArray {
 
     public static <E> void print(E[] arr){
@@ -11,7 +26,7 @@ public class PrintArray {
         Integer[] intArray = { 1, 2, 3, 4, 5 };
         Double[] doubleArray = { 1.1, 2.2, 3.3, 4.4 };
         Character[] charArray = {'H','E','L','L','O'};
-        PrintArray.print(intArray);
+        //new PArray<>(intArray).print();
         PrintArray.print(doubleArray);
         PrintArray.print(charArray);
     }
